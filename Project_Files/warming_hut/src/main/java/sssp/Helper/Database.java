3e2451c7d001c03d13375f05824bfe9a15d0d342
DrDisturbance <95546416+DrDisturbance@users.noreply.github.com>
2024-02-29 22:17:45 -0700

    updates

--- Project_Files/warming_hut/src/main/java/sssp/Helper/Database.java
+++ Project_Files/warming_hut/src/main/java/sssp/Helper/Database.java
@@ -8,9 +8,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Database {
-    public Map<String, Map<String, String>> enrollmentForm;
     public Map<String, Map<String, String>> guests;
-    public Map<String, Map<String, String>> storage;
+    public Map<String, Map<String, Map<String, String>>> conflicts;
+    public Map<String, Map<String, String>> cubeStorage;
+    public Map<String, Map<String, String>> dayStorage;
+    public Map<String, Map<String, String>> equipment;
+    public Map<String, Map<String, Map<String, String>>> guestRoster;
+    //         GuestLockers  Locker_1   AssignedStaff   NA
+    public Map<String, Map<String, Map<String, String>>> lockers;
+
+    // need to update deepcopy and printdatabase when additions are made
+    
 
     public void printDatabase() {
         System.out.println("Enrollment Form:");
@@ -26,16 +34,29 @@ public class Database {
     // Method to perform a deep copy of the entire database
     public Database deepCopy() {
         Database copy = new Database();
-        copy.enrollmentForm = deepCopyMap(this.enrollmentForm);
-        copy.guests = deepCopyMap(this.guests);
-        copy.storage = deepCopyMap(this.storage);
+        copy.guests = deepCopyMap1(this.guests);
+        copy.conflicts = deepCopyMap2(this.conflicts);
+        copy.cubeStorage = deepCopyMap1(this.cubeStorage);
+        copy.dayStorage = deepCopyMap1(this.dayStorage);
+        copy.equipment = deepCopyMap1(this.equipment);
+        copy.guestRoster = deepCopyMap2(this.guestRoster);
+        copy.lockers = deepCopyMap2(this.lockers);
         return copy;
     }
 
     // Helper method for deep copying a map
-    private Map<String, Map<String, String>> deepCopyMap(Map<String, Map<String, String>> original) {
+    private Map<String, Map<String, String>> deepCopyMap1(Map<String, Map<String, String>> original) {
         Map<String, Map<String, String>> copy = new HashMap<>();
         for (Map.Entry<String, Map<String, String>> entry : original.entrySet()) {
+            copy.put(entry.getKey(), new HashMap<>(entry.getValue()));
+        }
+        return copy;
+    }
+
+    // Helper method for deep copying a map
+    private Map<String, Map<String, Map<String, String>>> deepCopyMap2(Map<String, Map<String, Map<String, String>>> original) {
+        Map<String, Map<String, String>> copy = new HashMap<>();
+        for (Map.Entry<String,Map<String, Map<String, String>>> entry : original.entrySet()) {
             copy.put(entry.getKey(), new HashMap<>(entry.getValue()));
         }
         return copy;
