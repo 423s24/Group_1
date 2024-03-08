bf6bae08abd1ab9b29b8113de81d6ad53a589c38
Jake <98206654+SejongKadonk@users.noreply.github.com>
2024-03-07 22:11:55 -0700

    basic test framework started

---
+++ Project_Files/warming_hut/src/test/java/BasicTestTests.java
@@ -0,0 +1,17 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BasicTestTests {
+
+    @Test
+    public void shouldMigrateASimpleTest() {
+        Assert.assertEquals("expected", "expected");
+    }
+/*    @Test
+    @Ignore
+    public void shouldMigrateIgnoreTestToDisabledTest() {
+// This test is Ignored in JUnit 4 and should be Disabled in JUnit 5"
+    }*/
+}
